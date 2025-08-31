576f6e84ebe0c60029819a6d078414fab134f186
aidatar <93160007+aidatar@users.noreply.github.com>
2025-08-31 18:48:11 +0000

    Deploying to gh-pages from @ aidatar/aidatar.github.io@6f11f980fadeeaea3a8b9571bd65cd2d48998762 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -11,7 +11,7 @@ ninja.data = [{
     },
   },{id: "nav-cv",
           title: "cv",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
+          description: "",
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
@@ -419,7 +419,7 @@ ninja.data = [{
               window.location.href = "/projects/persian_jsoap/";
             },},{id: "projects-photography",
           title: 'Photography',
-          description: "A series capturing overlooked objects encountered while walking.",
+          description: "",
           section: "Projects",handler: () => {
               window.location.href = "/projects/photography/";
             },},{
