4a932fd006fbf4848f58294ac14a404f1a0905a5
aidakoosha <s2184935@ed.ac.uk>
2026-07-16 10:38:23 +0100

    post grad

---
+++ assets/js/search-data.js
@@ -0,0 +1,96 @@
+// get the ninja-keys element
+const ninja = document.querySelector('ninja-keys');
+
+// add the home and posts menu items
+ninja.data = [{
+    id: "nav-about",
+    title: "about",
+    section: "Navigation",
+    handler: () => {
+      window.location.href = "/";
+    },
+  },{id: "nav-research-outputs",
+          title: "research outputs",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/publications/";
+          },
+        },{id: "nav-projects",
+          title: "projects",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/projects/";
+          },
+        },{id: "news-i-presented-my-fourth-and-final-annual-review-poster-at-the-ukri-ai-cdts-in-nlp-joint-partners-summit-in-edinburgh",
+          title: 'I presented my fourth and final annual review poster at the UKRI AI...',
+          description: "",
+          section: "News",},{id: "news-i-m-pleased-to-share-that-i-passed-my-fourth-and-final-anual-review-with-very-good",
+          title: 'I’m pleased to share that I passed my fourth and final anual review...',
+          description: "",
+          section: "News",},{id: "news-i-will-give-an-oral-presentation-of-our-latest-work-at-the-british-psychological-society-scottish-branch-postgraduate-annual-conference-2025-in-late-september",
+          title: 'I will give an oral presentation of our latest work at the British...',
+          description: "",
+          section: "News",},{id: "news-starting-october-21st-2025-i-will-be-on-a-research-visit-at-swansea-university-s-computational-foundry-to-collaborate-with-dr-julian-hough-on-incremental-disfluency-detection",
+          title: 'Starting October 21st 2025, I will be on a research visit at Swansea...',
+          description: "",
+          section: "News",},{id: "news-i-submitted-my-doctoral-thesis-titled-written-difluencies-and-nonliteral-meaning-in-natural-language-processing-evidence-from-behavioral-and-computational-studies-looking-forward-to-my-pre-viva-talk-and-viva-voce-on-april-17th",
+          title: 'I submitted my doctoral thesis titled Written Difluencies and Nonliteral Meaning in Natural...',
+          description: "",
+          section: "News",},{id: "projects-multilingual-disfluencies",
+          title: 'Multilingual Disfluencies',
+          description: "UPCOMING - Collaboration with Julian Hough on multilingual disfluencies using DUEL and STIR/deep_disfluency.",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/ml_disfluencies/";
+            },},{id: "projects-persian-j-soap-ii-interview-package",
+          title: 'Persian J-SOAP-II Interview Package',
+          description: "ARCHIVED - Curation of a Persian version of J-SOAP-II for assessing juvenile sex offenders.",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/persian_jsoap/";
+            },},{id: "projects-give-me-the-camera",
+          title: 'Give me the camera.',
+          description: "",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/photography/";
+            },},{
+        id: 'social-linkedin',
+        title: 'LinkedIn',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://www.linkedin.com/in/f-a-s-tarighat", "_blank");
+        },
+      },{
+        id: 'social-scholar',
+        title: 'Google Scholar',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://scholar.google.com/citations?user=JBIFAB0AAAAJ", "_blank");
+        },
+      },{
+      id: 'light-theme',
+      title: 'Change theme to light',
+      description: 'Change the theme of the site to Light',
+      section: 'Theme',
+      handler: () => {
+        setThemeSetting("light");
+      },
+    },
+    {
+      id: 'dark-theme',
+      title: 'Change theme to dark',
+      description: 'Change the theme of the site to Dark',
+      section: 'Theme',
+      handler: () => {
+        setThemeSetting("dark");
+      },
+    },
+    {
+      id: 'system-theme',
+      title: 'Use system default theme',
+      description: 'Change the theme of the site to System Default',
+      section: 'Theme',
+      handler: () => {
+        setThemeSetting("system");
+      },
+    },];
